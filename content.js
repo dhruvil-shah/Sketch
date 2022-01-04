f5a94c5a5484f2711e9b09fd01c20df8aaf3f2a5
Dhruvil Shah <dhruvilshah2001@gmail.com>
2022-01-04 15:19:03 +0530

    Pen size done

--- content.js
+++ content.js
@@ -14,8 +14,7 @@ var s = function(sketch) {
 
   sketch.draw = function() {
     sketch.stroke(10);
-    // sketch.background(100);
-    sketch.strokeWeight(1);
+    sketch.strokeWeight(sketch.size!=null ?sketch.size:1);
     sketch.smooth(10);
     if (sketch.mouseIsPressed) {
       sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
@@ -25,9 +24,30 @@ var s = function(sketch) {
   sketch.toggleErase=function() {
     sketch.erase();
   }
-  sketch.toggleErase1=function(){
+  sketch.pen1=function(){
     sketch.noErase();
+    sketch.size=1;
+    sketch.draw();
   }
+  sketch.pen2=function(){
+    sketch.noErase();
+    sketch.size=5;
+    sketch.draw();
+  }
+
+  sketch.pen3=function(){
+    sketch.noErase();
+    sketch.size=10;
+    sketch.draw();
+  }
+
+  sketch.pen4=function(){
+    sketch.noErase();
+    sketch.size=20;
+    sketch.draw();
+  }
+
+
 };
 
 
@@ -35,14 +55,26 @@ var myp5 = new p5(s);
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(request);
+    
     if( request.message === "erase" ) {
        console.log("Recieved Erase");
        myp5.toggleErase();
          }
-    else if(request.message==="no_erase"){
-      console.log("No Erase Recieved");
-      myp5.toggleErase1();
-      
+    else if(request.message==="pen1"){
+      console.log("pen1");
+      myp5.pen1();
+    }
+    else if(request.message==="pen2"){
+      console.log("pen2");
+      myp5.pen2();
+    }
+    else if(request.message==="pen3"){
+      console.log("pen3");
+      myp5.pen3();
+    }
+    else if(request.message==="pen4"){
+      console.log("pen4");
+      myp5.pen4();
     }
   }
 );
--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 function erase() {
     console.log("erased called");
-    document.getElementById('no_erase').style['display']="inline";
-    document.getElementById('erase').style['display']="none";
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {message: "erase"});
    });
-}
-
-function noerase() {
-  console.log("noerased called");
-  document.getElementById('no_erase').style['display']="none";
-  document.getElementById('erase').style['display']="inline";
+  }
+  
+  function pen1() {
+  console.log("pen1 called");
   chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
   var activeTab = tabs[0];
-  chrome.tabs.sendMessage(activeTab.id, {message: "no_erase"});
- });
-}
+  chrome.tabs.sendMessage(activeTab.id, {message: "pen1"});
+  });
+  }
+
+  function pen2() {
+    console.log("pen2 called");
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {message: "pen2"});
+    });
+    }
+
+    function pen3() {
+        console.log("pen3 called");
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {message: "pen3"});
+        });
+        }
+
+        function pen4() {
+            console.log("pen4 called");
+            chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {message: "pen4"});
+            });
+            }
+
   document.getElementById("erase").addEventListener("click", erase);
-  document.getElementById("no_erase").addEventListener("click", noerase);
+
+
+  document.getElementById("pen1").addEventListener("click", pen1);
+  document.getElementById("pen2").addEventListener("click", pen2);
+  document.getElementById("pen3").addEventListener("click", pen3);
+  document.getElementById("pen4").addEventListener("click", pen4);
+  
